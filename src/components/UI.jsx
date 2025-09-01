2b082c4244a8fb0cfdcb1fb2c3aae9fcf90cd7e7
snepsnepy <snepysneptest@gmail.com>
2025-09-01 16:37:46 +0300

    Added title and favicon

--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -85,55 +85,10 @@ export const UI = () => {
         </div>
       </main>
 
-      <div className="fixed items-center select-none">
-        <div className="relative">
-          <div className="bg-white/0  animate-horizontal-scroll flex items-center gap-8 w-max px-8">
-            <h1 className="shrink-0 text-white text-10xl font-black ">
-              Snep Software
-            </h1>
-            <h2 className="shrink-0 text-white text-8xl italic font-light">
-              Your Text Here
-            </h2>
-            <h2 className="shrink-0 text-white text-12xl font-bold">
-              Flipbook
-            </h2>
-            <h2 className="shrink-0 text-transparent text-12xl font-bold italic outline-text">
-              Ultimate Guide
-            </h2>
-            <h2 className="shrink-0 text-white text-9xl font-extralight italic">
-              3D
-            </h2>
-            <h2 className="shrink-0 text-white text-13xl font-bold">
-              Your Text Here
-            </h2>
-            <h2 className="shrink-0 text-transparent text-13xl font-bold outline-text italic">
-              Creative
-            </h2>
-          </div>
-          <div className="absolute top-0 left-0 bg-white/0 animate-horizontal-scroll-2 flex items-center gap-8 px-8 w-max">
-            <h1 className="shrink-0 text-white text-10xl font-black ">
-              Snep Software
-            </h1>
-            <h2 className="shrink-0 text-white text-8xl italic font-light">
-              React Three Fiber
-            </h2>
-            <h2 className="shrink-0 text-white text-12xl font-bold">
-              Three.js
-            </h2>
-            <h2 className="shrink-0 text-transparent text-12xl font-bold italic outline-text">
-              Flipbook
-            </h2>
-            <h2 className="shrink-0 text-white text-9xl font-extralight italic">
-              3D
-            </h2>
-            <h2 className="shrink-0 text-white text-13xl font-bold">
-              Practice
-            </h2>
-            <h2 className="shrink-0 text-transparent text-13xl font-bold outline-text italic">
-              Creative
-            </h2>
-          </div>
-        </div>
+      <div className="fixed items-center select-none w-full text-center">
+        <h1 className="shrink-0 text-white text-5xl lg:text-9xl font-poppins font-semibold pt-4">
+          Your Flipbook Title
+        </h1>
       </div>
     </>
   );
